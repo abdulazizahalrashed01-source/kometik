0a781625c11ef253ab9f35ed42e1237f27ffcd58
aziz <abd.alrashed94@gmail.com>
2026-09-17 14:09:20 +0300

    Make store pages dynamic

--- src/app/(store)/layout.tsx
+++ src/app/(store)/layout.tsx
@@ -1,4 +1,6 @@
-﻿import Header from "@/components/Header";
+﻿export const dynamic = "force-dynamic";
+
+import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 import BackToTop from "@/components/BackToTop";
 
